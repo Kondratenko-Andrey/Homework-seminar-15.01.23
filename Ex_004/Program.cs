386d50716b5a862a94e7d0306308a453495614ce
Andrey Kondratenko <andrey92kondr@gmail.com>
2023-01-21 16:16:13 +0700

    Added complete homework (seminar 15.01.23)

---
+++ Ex_004/Program.cs
@@ -0,0 +1,54 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+Console.WriteLine("Для определения расстояния между двумя точками для начала введите в каких измерениях будем работать.");
+Console.Write("Двухмерное (вводим 2D) или трёхмерное (вводим 3D): ");
+string space = Console.ReadLine() ?? "0";
+
+int coordinate_x1 = 0, coordinate_y1 = 0, coordinate_z1 = 0,
+    coordinate_x2 = 0, coordinate_y2 = 0, coordinate_z2 = 0;
+
+double distance = 0;
+
+if(space.ToLower() == "2d")
+{
+    Console.WriteLine("Отлично, работаем в двухмерном пространстве.");
+    Console.WriteLine("Введите значение координат по оси абсцисс (х1) и по оси ординат (у1) для первой точки.");
+    Console.Write("х1 = ");
+    coordinate_x1 =int.Parse(Console.ReadLine());
+    Console.Write("y1 = ");
+    coordinate_y1 =int.Parse(Console.ReadLine());
+    
+    Console.WriteLine("Введите значение координат по оси абсцисс (х2) и по оси ординат (у2) для второй точки.");
+    Console.Write("х2 = ");
+    coordinate_x2 =int.Parse(Console.ReadLine());
+    Console.Write("y2 = ");
+    coordinate_y2 =int.Parse(Console.ReadLine());
+
+    distance = Math.Sqrt(Math.Pow((coordinate_x2 - coordinate_x1),2) +  Math.Pow((coordinate_y2 - coordinate_y1),2));
+
+    Console.Write("Расстояние на двухмерной плоскости между двумя точками, с указанными Вами координатами равно " + distance);
+}
+
+if(space.ToLower() == "3d")
+{
+    Console.WriteLine("Отлично, работаем в трёхмерном пространстве.");
+    Console.WriteLine("Введите значение координат по оси абсцисс (х1), по оси ординат (у1) и по оси аппликат (z1) для первой точки.");
+    Console.Write("х1 = ");
+    coordinate_x1 =int.Parse(Console.ReadLine());
+    Console.Write("y1 = ");
+    coordinate_y1 =int.Parse(Console.ReadLine());
+    Console.Write("z1 = ");
+    coordinate_z1 =int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите значение координат по оси абсцисс (х2), по оси ординат (у2) и по оси аппликат (z2) для второй точки.");
+    Console.Write("х2 = ");
+    coordinate_x2 =int.Parse(Console.ReadLine());
+    Console.Write("y2 = ");
+    coordinate_y2 =int.Parse(Console.ReadLine());
+    Console.Write("z2 = ");
+    coordinate_z2 =int.Parse(Console.ReadLine());
+
+    distance = Math.Sqrt(Math.Pow((coordinate_x2 - coordinate_x1),2) +  Math.Pow((coordinate_y2 - coordinate_y1),2) +  Math.Pow((coordinate_z2 - coordinate_z1),2));
+
+    Console.Write("Расстояние на трёхмерной плоскости между двумя точками, с указанными Вами координатами равно " + distance);
+}
+
---
+++ Ex_02/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Введите число num_1: ");
+int num_1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите число num_2: ");
+int num_2 = int.Parse(Console.ReadLine() ?? "0");
+
+if(num_1 == num_2 * num_2) Console.Write("Число num_1 является квадратом числа num_2 !");
+    else if(num_2 == num_1 * num_1) Console.Write("Число num_2 является квадратом числа num_1 !");
+        else Console.Write("Ни одно из заданных чисел (num_1, num_2) не являются квадратом другого !");
